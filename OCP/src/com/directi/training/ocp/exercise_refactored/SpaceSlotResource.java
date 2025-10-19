3fde4518c927a622930e8ca6d488ffb54f11dc09
Wejdene Chamroukhi <wejdene24chamroukhi@gmail.com>
2025-10-19 11:30:01 +0100

    Add OCP refactored implementation with Resource interface and implementations

---
+++ OCP/src/com/directi/training/ocp/exercise_refactored/SpaceSlotResource.java
@@ -0,0 +1,26 @@
+package com.directi.training.ocp.exercise_refactored;
+
+public class SpaceSlotResource implements Resource {
+    @Override
+    public int allocate() {
+        int resourceId = findFreeSpaceSlot();
+        markSpaceSlotBusy(resourceId);
+        return resourceId;
+    }
+
+    @Override
+    public void free(int resourceId) {
+        markSpaceSlotFree(resourceId);
+    }
+
+    private int findFreeSpaceSlot() {
+        return 0;
+    }
+
+    private void markSpaceSlotFree(int resourceId) {
+    }
+
+    private void markSpaceSlotBusy(int resourceId) {
+    }
+    
+}
